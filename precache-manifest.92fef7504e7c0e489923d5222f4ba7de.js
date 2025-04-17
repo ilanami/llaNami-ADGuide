e34feea3a128d537e8089d1e89f6723094d136bd
IlaNami <ilanami@pm.me>
2025-04-17 20:25:55 +0200

    Updates

---
+++ precache-manifest.92fef7504e7c0e489923d5222f4ba7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f523f78ea81810f6893317db1758f14",
+    "url": "/llaNami-ADGuide/index.html"
+  },
+  {
+    "revision": "f1bdfde23b768049dfd5",
+    "url": "/llaNami-ADGuide/static/css/main.efe31589.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/llaNami-ADGuide/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/llaNami-ADGuide/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "f1bdfde23b768049dfd5",
+    "url": "/llaNami-ADGuide/static/js/main.91b06186.chunk.js"
+  },
+  {
+    "revision": "1baacec462630fa0826f",
+    "url": "/llaNami-ADGuide/static/js/runtime~main.b06c7223.js"
+  }
+]);
