57129e1230ef11306a5e56391750465615e04174
IlaNami <ilanami@pm.me>
2025-04-17 19:56:10 +0200

    Subida inicial del proyecto llaNami-ADGuide
    Primer commit con la estructura base del proyecto de pentesting para Active Directory

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ADPentestGuide from './components/ADPentestGuide';
+
+function App() {
+  return (
+    <div className="App">
+      <ADPentestGuide />
+    </div>
+  );
+}
+
+export default App;
